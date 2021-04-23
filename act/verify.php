3dd91f9e32c15badc7386008a09cf96afc471d73
Verona Reshani <VeronaReshani@gmail.com>
2021-04-23 09:48:41 +0200

    //

--- act/verify.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$loginAdminUser = array("username" => "vera", "password" => "vera", 'role' => '1');
-$loginSimpleUser = array("username" => "test", "password" => "test", 'role' => '0');
-
-$users = array($loginAdminUser, $loginSimpleUser);
-
-?>
